a70edf235381b077a2c7d9140807a81279f12831
Julian Metzler <git@mezgr.de>
2023-08-04 00:18:34 +0200

    Actually display trains now

--- python/run.py
+++ python/run.py
@@ -20,7 +20,8 @@ import time
 
 from c3toc import C3TOCAPI
 from pprint import pprint
-#from pyfis.aegmis import MIS1MatrixDisplay
+from pyfis.aegmis import MIS1MatrixDisplay
+from pyfis.aegmis.exceptions import CommunicationError
 
 from _config import *
 from text_renderer import TextRenderer
@@ -29,8 +30,8 @@ from rgb_controller import RGBController
 
 DISPLAY_MODES = [
     "arr_dep_eta",
-    "map",
-    "pride"
+    #"map",
+    #"pride"
 ]
 
 
@@ -50,16 +51,19 @@ def main():
     
     api = C3TOCAPI()
     renderer = TextRenderer("../fonts")
-    #display = MIS1MatrixDisplay(CONFIG_LCD_PORT, baudrate=115200, debug=False)
+    display = MIS1MatrixDisplay(CONFIG_LCD_PORT, baudrate=115200, use_rts=False, debug=False)
     #rgb = RGBController(CONFIG_RGB_PORT)
     
     tracks = api.get_tracks()
     track_length = sorted(tracks['waypoints'].values(), key=lambda e: e['trackmarker'])[-1]['trackmarker']
     print("Track length: {}".format(track_length))
     
-    #display.reset()
-    #time.sleep(1)
-    """display.set_config(
+    try:
+        display.reset()
+    except CommunicationError:
+        pass
+    time.sleep(1)
+    display.set_config(
         lcd_module=0,
         num_lcds=3,
         x=0,
@@ -71,12 +75,12 @@ def main():
         is_master=False,
         protocol_timeout=600,
         response_delay=0
-    )"""
-    #display.become_master()
+    )
+    display.become_master()
     
     while True:
         try:
-            #display.delete_page(secondary_page)
+            display.delete_page(secondary_page)
             #rgb.clear()
             page, secondary_page = secondary_page, page
             print("Handling mode: " + mode)
@@ -98,20 +102,32 @@ def main():
             # Handle displaying the required content
             if mode == "arr_dep_eta":
                 if train_info:
-                    pass
+                    items = sorted(train_info.items(), key=lambda i: i[1]['eta'] or datetime.datetime(2070, 1, 1, 0, 0, 0))
+                    for i, (name, data) in enumerate(items):
+                        if data['eta'] is not None:
+                            eta_str = str(round(max((data['eta'] - utcnow).total_seconds(), 0) / 60))
+                        else:
+                            eta_str = "???"
+                        y_base = i * 16
+                        line_image = renderer.render_text(width=28, height=16, pad_left=0, pad_top=0, font="12_DBLCD", size=0, halign='center', valign='middle', inverted=False, spacing=1, char_width=None, text=name[:2].upper())
+                        display.image(page, 0, y_base, line_image)
+                        dest_image = renderer.render_text(width=130, height=16, pad_left=0, pad_top=0, font="12_DBLCD", size=0, halign='left', valign='middle', inverted=True, spacing=1, char_width=None, text=name)
+                        display.image(page, 32, y_base, dest_image)
+                        eta_image = renderer.render_text(width=28, height=16, pad_left=0, pad_top=0, font="12_DBLCD", size=0, halign='right', valign='middle', inverted=True, spacing=1, char_width=None, text=eta_str)
+                        display.image(page, 260, y_base, eta_image)
                 else:
                     no_dep_img = renderer.render_text(width=display_width, height=32, pad_left=0, pad_top=0, font="21_DBLCD", size=0, halign='center', valign='middle', inverted=True, spacing=2, char_width=None, text="Kein Zugverkehr")
-                    #display.image(page, 0, 16, no_dep_img)
+                    display.image(page, 0, 16, no_dep_img)
                     
             
             # Process any messages from the display and check for errors
-            """while True:
+            while True:
                 response = display.send_tx_request()
                 if response[0] == 0x15:
                     break
-                display.check_error(response)"""
+                display.check_error(response)
             
-            #display.set_page(page)
+            display.set_page(page)
             #rgb.update()
             mode_index += 1
             if mode_index >= len(DISPLAY_MODES):
