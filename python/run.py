df14f527d94cf5f8ce7aa0cf0adf0ec84cd34044
Julian Metzler <git@mezgr.de>
2023-08-08 22:52:03 +0200

    Add pretalx schedule

--- python/run.py
+++ python/run.py
@@ -37,9 +37,33 @@ from gcm_controller import GCMController
 
 DISPLAY_MODES = [
     #"arr_dep_eta",
-    #"pretalx",
-    "pride"
+    "pretalx",
+    #"pride"
 ]
+
+TRACK_CODES = {
+    "Digitalcourage": "DC",
+    "Live Music": "LM",
+    "Bits & Bäume": "BB",
+    "DJ Set": "DJ",
+    "CCC": "C",
+    "Nerds der OberRheinischen Tiefebene und der xHain (N\\:O:R:T:x)": "NX",
+    "Entertainment": "E",
+    "Performance": "P",
+    "Milliways": "MW"
+}
+
+TRACK_COLORS = {
+    "DC": 0xfbc617,
+    "LM": 0x9d9d9d,
+    "BB": 0x81c854,
+    "DJ": 0x9d9d9d,
+    "C":  0xfb48c4,
+    "NX": 0x003a3e,
+    "E":  0x1a36cd,
+    "P":  0x9d9d9d,
+    "MW": 0x3cacd7
+}
 
 
 # Pretalx event filters
@@ -109,7 +133,7 @@ def main():
     secondary_page = 2
     display_width = 3 * 96
     display_height = 64
-    page_interval = 10
+    page_interval = 20 # Page switch interval in seconds (roughly)
     
     eta_lookback = 10 # How many minutes of past train positions to consider for ETA
     eta_max_jump = 30 # Maximum ETA jump in seconds
@@ -119,14 +143,15 @@ def main():
     toc = C3TOCAPI()
     pretalx = PretalxAPI("https://pretalx.c3voc.de/camp2023/schedule/export/schedule.json")
     renderer = TextRenderer("../fonts")
-    #display = MIS1MatrixDisplay(CONFIG_LCD_PORT, baudrate=115200, use_rts=False, debug=False)
-    #gcm = GCMController(CONFIG_GCM_PORT)
+    display = MIS1MatrixDisplay(CONFIG_LCD_PORT, baudrate=115200, use_rts=False, debug=False)
+    gcm = GCMController(CONFIG_GCM_PORT)
+    time.sleep(3)
     
     tracks = toc.get_tracks()
     track_length = sorted(tracks['waypoints'].values(), key=lambda e: e['trackmarker'])[-1]['trackmarker']
     print("Track length: {}".format(track_length))
     
-    """try:
+    try:
         display.reset()
     except CommunicationError:
         pass
@@ -144,22 +169,22 @@ def main():
         protocol_timeout=600,
         response_delay=0
     )
-    display.become_master()"""
+    display.become_master()
     
     while True:
         try:
-            #display.delete_page(secondary_page)
-            #gcm.clear()
+            display.delete_page(secondary_page)
+            gcm.clear()
             page, secondary_page = secondary_page, page
             print("Handling mode: " + mode)
             utcnow = datetime.datetime.utcnow()
+            now = datetime.datetime.now()
             
             # Handle all background calculations and data operations
             
             # c3toc API #######################################################
             # Get trains from API and calculate ETAs
             # This is run more frequently than it is displayed to keep the ETA more accurate
-            """
             train_info = toc.get_train_info(display_trackmarker, eta_lookback, eta_max_jump, trackmarker_delta_arrived, track_length)
             
             for name, info in train_info.items():
@@ -169,14 +194,11 @@ def main():
                     delta = (info['eta'] - utcnow).total_seconds()
                     print("{name} will arrive at trackmarker {trackmarker} in {seconds} seconds, at {time} UTC".format(name=name, trackmarker=display_trackmarker, seconds=delta, time=info['eta'].strftime("%H:%M:%S")))
                 pprint(info)
-            """
             ###################################################################
             
             
             # Handle displaying the required content
             if mode == "arr_dep_eta":
-                pass
-                """
                 if train_info:
                     items = sorted(train_info.items(), key=lambda i: i[1]['eta'] or datetime.datetime(2070, 1, 1, 0, 0, 0))
                     for i, (name, data) in enumerate(items):
@@ -187,30 +209,86 @@ def main():
                         y_base = i * 16
                         for sector in range(8):
                             gcm.set_sector(y_base // 2 + sector, 0xffffff)
-                        line_image = renderer.render_text(width=28, height=16, pad_left=0, pad_top=0, font="12_DBLCD", size=0, halign='center', valign='middle', inverted=False, spacing=1, char_width=None, text=name[:2].upper())
+                        line_image = renderer.render_text(width=24, height=16, pad_left=0, pad_top=0, font="12_DBLCD", size=0, halign='center', valign='middle', inverted=False, spacing=1, char_width=None, text=name[:2].upper())
                         display.image(page, 0, y_base, line_image)
                         dest_image = renderer.render_text(width=130, height=16, pad_left=0, pad_top=0, font="12_DBLCD", size=0, halign='left', valign='middle', inverted=True, spacing=1, char_width=None, text=name)
                         display.image(page, 32, y_base, dest_image)
                         eta_image = renderer.render_text(width=28, height=16, pad_left=0, pad_top=0, font="12_DBLCD", size=0, halign='right', valign='middle', inverted=True, spacing=1, char_width=None, text=eta_str)
                         display.image(page, 260, y_base, eta_image)
                 else:
-                    no_dep_img = renderer.render_text(width=display_width, height=32, pad_left=0, pad_top=0, font="21_DBLCD", size=0, halign='center', valign='middle', inverted=True, spacing=2, char_width=None, text="Kein Zugverkehr")
+                    no_dep_img = renderer.render_text(width=display_width, height=32, pad_left=0, pad_top=0, font="21_DBLCD", size=0, halign='center', valign='middle', inverted=True, spacing=2, char_width=None, text="No Departures")
                     display.image(page, 0, 16, no_dep_img)
-                """
             elif mode == "pretalx":
+                # Display header
+                track_image = renderer.render_text(width=28, height=7, pad_left=0, pad_top=0, font="7_DBLCD", size=0, halign='left', valign='top', inverted=True, spacing=1, char_width=None, text="Trck")
+                location_image = renderer.render_text(width=68, height=7, pad_left=0, pad_top=0, font="7_DBLCD", size=0, halign='left', valign='top', inverted=True, spacing=1, char_width=None, text="Location")
+                title_image = renderer.render_text(width=32, height=7, pad_left=0, pad_top=0, font="7_DBLCD", size=0, halign='left', valign='top', inverted=True, spacing=1, char_width=None, text="Title")
+                time_image = renderer.render_text(width=50, height=7, pad_left=0, pad_top=0, font="7_DBLCD", size=0, halign='right', valign='top', inverted=True, spacing=1, char_width=None, text="Starts in")
+                display.image(page, 0, 0, track_image)
+                display.image(page, 28, 0, location_image)
+                display.image(page, 96, 0, title_image)
+                display.image(page, 238, 0, time_image)
+                display.fill_area(page, x=0, y=8, width=288, height=1, state=1)
+                for i in range(5):
+                    gcm.set_sector(i, 0xffffff)
+
                 # Get schedule from pretalx
                 events = pretalx.get_all_events()
+
+                #tracks = list(set([event['track'] for event in events]))
+                #pprint(tracks)
                 
                 # Filter out all events longer then 2 hours
                 events = filter(lambda event: _max_duration(event, 2, 0), events)
                 
                 # Filter out all events that are finished
                 events = filter(lambda event: _ongoing_or_future(event), events)
-                
                 events = list(events)
-                pprint(["{date} {duration} {title}".format(**event) for event in events[:10]])
+
+                if events:
+                    for i, event in enumerate(events[:3]):
+                        start = dateutil.parser.isoparse(event['date']).replace(tzinfo=None)
+                        delta = start - now
+                        seconds = round(delta.total_seconds())
+                        if seconds < 0:
+                            seconds = 0
+                        if seconds >= 3600:
+                            time_text = "{}h{}m".format(seconds // 3600, round((seconds % 3600) / 60))
+                        else:
+                            time_text = "{}m".format(round((seconds % 3600) / 60))
+
+                        track_code = TRACK_CODES.get(event['track'], event['track'].upper()[:2])
+                        track_color = TRACK_COLORS.get(track_code, 0xffffff)
+
+                        y_base = 12 + i * 16
+                        for r in range(8):
+                            gcm.set_sector(y_base // 2 + r, track_color)
+
+                        track_image = renderer.render_text(width=24, height=16, pad_left=0, pad_top=0, font="10S_DBLCD", size=0, halign='center', valign='middle', inverted=False, spacing=1, char_width=None, text=track_code)
+                        room_image = renderer.render_text(width=500, height=16, pad_left=0, pad_top=3, font="7_DBLCD", size=0, halign='left', valign='top', inverted=True, spacing=1, char_width=None, text=event['room'])
+                        title_image = renderer.render_text(width=1000, height=16, pad_left=0, pad_top=0, font="10S_DBLCD", size=0, halign='left', valign='top', inverted=True, spacing=1, char_width=None, text=event['title'])
+                        time_image = renderer.render_text(width=50, height=16, pad_left=0, pad_top=0, font="10S_DBLCD", size=0, halign='right', valign='top', inverted=True, spacing=1, char_width=None, text=time_text)
+                        
+                        room_bbox = room_image.getbbox()
+                        room_image = room_image.crop((0, 0, room_bbox[2], room_bbox[3]))
+                        title_bbox = title_image.getbbox()
+                        title_image = title_image.crop((0, 0, title_bbox[2], title_bbox[3]))
+                        
+                        display.image(page, 0, y_base, track_image)
+                        if room_image.size[0] > 64:
+                            display.scroll_image(i*2, page, 28, y_base, 64, room_image, extra_whitespace=25)
+                        else:
+                            display.image(page, 28, y_base, room_image)
+                        if title_image.size[0] > 140:
+                            display.scroll_image(i*2+1, page, 96, y_base, 140, title_image, extra_whitespace=50)
+                        else:
+                            display.image(page, 96, y_base, title_image)
+                        display.image(page, 238, y_base, time_image)
+                else:
+                    no_evt_img = renderer.render_text(width=display_width, height=32, pad_left=0, pad_top=0, font="21_DBLCD", size=0, halign='center', valign='middle', inverted=True, spacing=2, char_width=None, text="No Events")
+                    display.image(page, 0, 16, no_evt_img)
             elif mode == "pride":
-                display.fill_area(page, x=0, y=0, width=28, height=64, state=1)
+                display.fill_area(page, x=0, y=0, width=24, height=64, state=1)
                 flags = [file for file in os.listdir("../flags") if not file.endswith("json")]
                 flag = random.choice(flags)
                 flag_path = os.path.join("../flags", flag)
@@ -221,23 +299,21 @@ def main():
                     info = json.load(f)
                 for i, color in enumerate(sectors):
                     gcm.set_sector(i, color)
-                name_image = renderer.render_text(width=256, height=16, pad_left=0, pad_top=0, font="17_DBLCD", size=0, halign='left', valign='middle', inverted=True, spacing=2, char_width=None, text=info['name'])
+                name_image = renderer.render_text(width=256, height=24, pad_left=0, pad_top=0, font="14S_DBLCD", size=0, halign='left', valign='middle', inverted=True, spacing=2, char_width=None, text="Pride Flags: " + info['name'])
                 display.image(page, 32, 0, name_image)
-                info_image = renderer.render_multiline_text(width=256, height=44, pad_left=0, pad_top=0, font="10S_DBLCD", size=0, halign='left', valign='bottom', inverted=True, h_spacing=1, v_spacing=3, char_width=None, text=info['info'], auto_wrap=True, break_words=False)
-                display.image(page, 32, 20, info_image)
+                info_image = renderer.render_multiline_text(width=256, height=40, pad_left=0, pad_top=0, font="10S_DBLCD", size=0, halign='left', valign='bottom', inverted=True, h_spacing=1, v_spacing=3, char_width=None, text=info['info'], auto_wrap=True, break_words=False)
+                display.image(page, 32, 24, info_image)
                     
             
             # Process any messages from the display and check for errors
-            """
             while True:
                 response = display.send_tx_request()
                 if response[0] == 0x15:
                     break
                 display.check_error(response)
-            """
             
-            #display.set_page(page)
-            #gcm.update()
+            display.set_page(page)
+            gcm.update()
             mode_index += 1
             if mode_index >= len(DISPLAY_MODES):
                 mode_index = 0
