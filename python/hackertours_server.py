bc37c80c3a1c565daddc0c93b6cd36586c9060c8
Julian Metzler <git@mezgr.de>
2023-12-20 22:23:24 +0100

    Flask: Disable debug mode

--- python/hackertours_server.py
+++ python/hackertours_server.py
@@ -54,4 +54,4 @@ def index():
     return render_template_string(html, content=content)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=False, host='0.0.0.0')
